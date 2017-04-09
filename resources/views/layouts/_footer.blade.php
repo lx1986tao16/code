2e51c326b1dd553f0072ad53a485384e41d3d820
谭涛 <ganning@pokerlink.cn>
2017-04-09 21:15:08 +0800

    flush user signup

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,8 +1,8 @@
 <div class="col-md-12">
     <footer class="footer">
         <small class="slogon">
-            <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
-            <a href="http://estgroupe.com/">优帆远扬 | 创造不息，交付不止</a>
+            <img class="brand-icon" src="">
+            <a href="#">Tandy</a>
         </small>
         <nav>
             <ul>
