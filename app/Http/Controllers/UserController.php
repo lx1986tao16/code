9f70a8ef2eb47f2ef5dda1b2ba9a5338a54856fc
谭涛 <ganning@pokerlink.cn>
2017-04-11 15:34:18 +0800

    整合重置密码

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Auth;
+use Mail;
 use App\User;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -54,10 +55,24 @@ class UserController extends Controller
             'password'  =>  bcrypt($request->password),
         ]);
 
-        Auth::login($user);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
 
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show', [$user]);
+        return redirect('/');
+    }
+
+    public function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'lx1986tao16@126.com';
+        $name = 'Tandy';
+        $to = $user->email;
+        $subject = "感谢注册TandyCode应用！请确认你的邮箱";
+
+        Mail::send($view, $data, function($message) use ($from, $name, $to, $subject){
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
     }
 
     public function edit($id)
@@ -99,5 +114,18 @@ class UserController extends Controller
 
         session()->flash('success', "成功删除用户！");
         return back();
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜您，激活成功！');
+        return redirect()->route('users.show', [$user]);
     }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -20,6 +20,7 @@ class UserTableSeeder extends Seeder
         $user->email = "lx1986tao16@gmail.com";
         $user->password = bcrypt('admin123');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
