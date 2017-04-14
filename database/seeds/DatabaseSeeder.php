6aee44ba7c920284cb2c7eb70c3382008be55d58
谭涛 <ganning@pokerlink.cn>
2017-04-14 09:52:01 +0800

    整合关注与取消关注

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-         $this->call(UserTableSeeder::class);
-         $this->call(StatusesTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
+        $this->call('FollowersTableSeeder');
 
         Model::reguard();
     }
