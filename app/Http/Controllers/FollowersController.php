634f376bf711c7df56b0a08b30babf845a285ddd
谭涛 <ganning@pokerlink.cn>
2017-04-14 10:18:52 +0800

    整合关注与取消关注

--- app/Http/Controllers/FollowersController.php
+++ app/Http/Controllers/FollowersController.php
@@ -4,18 +4,46 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class FollowersController extends Controller
 {
-    public function store(Request $request)
+    public function __construct()
     {
-        
+        $this->middleware('auth', [
+            'store', 'destroy'
+        ]);
+    }
+
+    public function store($id)
+    {
+        $user = User::findOrFail($id);
+        if (Auth::user()->id === $user->id) {
+            return redirect('/');
+        }
+
+        if (!Auth::user()->isFollowing($id)) {
+            Auth::user()->follow($id);
+        }
+
+        return redirect()->route('users.show', $id);
     }
 
     public function destroy($id)
     {
+        $user = User::findOrFail($id);
+        $auth_user = Auth::user();
+        if ($auth_user->id === $user->id) {
+            return redirect('/');
+        }
+
+        if ($auth_user->isFollowing($id)) {
+            $auth_user->unfollow($id);
+        }
+
+        return redirect()->route('users.show', $id);
 
     }
 }
